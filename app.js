69bb5699dc26d3bc97b1f5ee0764da8b4fead034
khangohan21 <108592029+khangohan21@users.noreply.github.com>
2022-09-23 15:29:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const  express = require('express');
-const  app = express();
-app.get('/', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+const express = require('express');
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
-app.listen(process.env.PORT || 3000);
-console.log('Server ready')
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
